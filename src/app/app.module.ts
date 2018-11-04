92287397223e631caa74b36e8d4aff8cb7950c3a
alezanper <fabenavideso@gmail.com>
2018-11-04 18:34:56 -0500

    toolbar buttons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+
+
 @NgModule({
   declarations: [
     AppComponent,
     ToolbarComponent
   ],
   imports: [
+    MatToolbarModule,
+    MatButtonModule,
     BrowserModule
   ],
   providers: [],
